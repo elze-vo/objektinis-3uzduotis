c9a4cea070ddc279d0088e111ef6dc4dd0cb18b5
elze-vo <evoveriunaite@gmail.com>
2024-09-26 19:48:10 +0300

    Projekto išskaidymas - main.cpp
    - Pakeistas main.cpp failas po projekto išskaidymo į skirtingus failus

--- main.cpp
+++ main.cpp
@@ -1,166 +1,16 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <numeric>
+#include <vector>
 #include <iomanip>
 #include <algorithm>
-#include <fstream>
-#include <sstream>
+#include <limits>
+#include "Studentas.h"
+#include "Rezultatai.h"
+#include "utils.h"
+#include "student_input.h"
+#include "random_student_input.h"
 
 using namespace std;
-
-struct Rezultatai {
-    vector<int> namuDarbuRezultatai;
-    int egzaminoRezultatas;
-};
-
-struct Studentas {
-    string vardas;
-    string pavarde;
-    Rezultatai rezultatai;
-};
-
-double calculateMedian(vector<int>& grades) {
-    sort(grades.begin(), grades.end());
-    size_t size = grades.size();
-    if (size == 0) return 0.0;
-    if (size % 2 == 0) {
-        return (grades[size / 2 - 1] + grades[size / 2]) / 2.0;
-    }
-    return grades[size / 2];
-}
-
-void skaitytiIsFailo(vector<Studentas>& studentai, const string& fileName) {
-    ifstream file(fileName);
-    if (!file) {
-        cout << "Nepavyko atidaryti failo: " << fileName << endl;
-        return;
-    }
-
-    string line;
-    getline(file, line);
-    istringstream headerStream(line);
-    string headerWord;
-    int numHomework = 0;
-
-    while (headerStream >> headerWord) {
-        if (headerWord.find("ND") != string::npos) {
-            numHomework++;
-        }
-    }
-
-    while (getline(file, line)) {
-        istringstream iss(line);
-        Studentas studentas;
-        int rezultatas;
-
-        iss >> studentas.vardas >> studentas.pavarde;
-
-        for (int i = 0; i < numHomework; ++i) {
-            if (iss >> rezultatas) {
-                studentas.rezultatai.namuDarbuRezultatai.push_back(rezultatas);
-            }
-        }
-
-        if (iss >> rezultatas) {
-            studentas.rezultatai.egzaminoRezultatas = rezultatas;
-        }
-
-        studentai.push_back(studentas);
-    }
-
-    file.close();
-}
-
-bool compareByLastName(const Studentas& a, const Studentas& b) {
-    return a.pavarde < b.pavarde;
-}
-
-void addStudentsManually(vector<Studentas>& studentai) {
-    int numberOfStudents;
-    cout << "Iveskite studentu kieki: ";
-    cin >> numberOfStudents;
-    cin.ignore();
-
-    for (int i = 0; i < numberOfStudents; ++i) {
-        Studentas studentas;
-        cout << "Iveskite " << i + 1 << "-ojo studento varda: ";
-        cin >> studentas.vardas;
-        cout << "Iveskite " << i + 1 << "-ojo studento pavarde: ";
-        cin >> studentas.pavarde;
-        cin.ignore();
-
-        cout << "Iveskite namu darbu rezultatus (spauskite Enter dukart norint baigti): " << endl;
-        string input;
-
-        while (true) {
-            cout << "Namu darbas: ";
-            getline(cin, input);
-
-            if (input.empty()) {
-                if (!studentas.rezultatai.namuDarbuRezultatai.empty()) {
-                    break;
-                }
-            }
-            else {
-                try {
-                    int grade = stoi(input);
-                    if (grade < 0 || grade > 10) {
-                        cout << "Namu darbas turi buti tarp 0 ir 10. Bandykite dar karta." << endl;
-                    }
-                    else {
-                        studentas.rezultatai.namuDarbuRezultatai.push_back(grade);
-                    }
-                }
-                catch (invalid_argument&) {
-                    cout << "Ivedete neteisinga reiksme. Bandykite dar karta." << endl;
-                }
-                catch (out_of_range&) {
-                    cout << "Ivedete reiksme, kuri yra per didele. Bandykite dar karta." << endl;
-                }
-            }
-        }
-
-        cout << "Iveskite egzamino rezultata: ";
-        while (true) {
-            cin >> studentas.rezultatai.egzaminoRezultatas;
-
-            if (studentas.rezultatai.egzaminoRezultatas < 0 || studentas.rezultatai.egzaminoRezultatas > 10) {
-                cout << "Egzamino rezultatas turi buti tarp 0 ir 10. Bandykite dar karta: ";
-            }
-            else {
-                cin.ignore();
-                break;
-            }
-        }
-
-        studentai.push_back(studentas);
-    }
-}
-
-void addStudentsRandomly(vector<Studentas>& studentai) {
-    int numHomework;
-    cout << "Iveskite namu darbu kieki: ";
-    cin >> numHomework;
-
-    int numberOfStudents;
-    cout << "Iveskite studentu kieki: ";
-    cin >> numberOfStudents;
-
-    for (int i = 0; i < numberOfStudents; ++i) {
-        Studentas studentas;
-        studentas.vardas = "Vardas" + to_string(i + 1);
-        studentas.pavarde = "Pavarde" + to_string(i + 1);
-
-        for (int j = 0; j < numHomework; ++j) {
-            int randomGrade = rand() % 11;
-            studentas.rezultatai.namuDarbuRezultatai.push_back(randomGrade);
-        }
-
-        studentas.rezultatai.egzaminoRezultatas = rand() % 11;
-        studentai.push_back(studentas);
-    }
-}
 
 int main() {
     vector<Studentas> studentai;
@@ -200,8 +50,19 @@ int main() {
         return 1;
     }
 
-    cout << "Pasirinkite (1 - Vidurkis, 2 - Mediana): ";
-    cin >> pasirinkimas;
+    do {
+        cout << "Pasirinkite (1 - Vidurkis, 2 - Mediana): ";
+        cin >> pasirinkimas;
+
+        if (cin.fail() || (pasirinkimas != 1 && pasirinkimas != 2)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+        }
+        else {
+            break;
+        }
+    } while (true);
 
     sort(studentai.begin(), studentai.end(), compareByLastName);
 
