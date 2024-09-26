a2ccf85400bbde77851060802bcf0a29cbd1da44
elze-vo <evoveriunaite@gmail.com>
2024-09-26 20:05:02 +0300

    Įvesties tikrinimas
    - Programa tikrina, ar studentų įvedimo būdo pasirinkimas yra tinkamas, kol jis būna tinkamas
    - Programa tikrina, ar įvestas failo pavadinimas yra tinkamas, kol jis būna tinkamas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <algorithm>
 #include <limits>
+#include <fstream>
 #include "Studentas.h"
 #include "Rezultatai.h"
 #include "utils.h"
@@ -16,20 +17,45 @@ int main() {
     vector<Studentas> studentai;
     int pasirinkimas;
 
-    cout << "Pasirinkite veiksmus:\n";
-    cout << "1 - Prideti studentus is failo\n";
-    cout << "2 - Prideti studentus ranka\n";
-    cout << "3 - Generuoti studentus atsitiktinai\n";
-    cout << "Iveskite pasirinkima: ";
-    cin >> pasirinkimas;
-    cin.ignore();
+    do {
+        cout << "Pasirinkite veiksmus:\n";
+        cout << "1 - Prideti studentus is failo\n";
+        cout << "2 - Prideti studentus ranka\n";
+        cout << "3 - Generuoti studentus atsitiktinai\n";
+        cout << "Iveskite pasirinkima: ";
+        cin >> pasirinkimas;
+
+        if (cin.fail() || (pasirinkimas < 1 || pasirinkimas > 3)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+        }
+        else {
+            cin.ignore();
+            break;
+        }
+    } while (true);
 
     switch (pasirinkimas) {
     case 1: {
         string failoPavadinimas;
-        cout << "Iveskite failo pavadinima (pvz., studentai.txt): ";
-        cin >> failoPavadinimas;
-        skaitytiIsFailo(studentai, failoPavadinimas);
+        ifstream inputFile;
+
+        do {
+            cout << "Iveskite failo pavadinima (pvz., studentai.txt): ";
+            cin >> failoPavadinimas;
+
+            inputFile.open(failoPavadinimas);
+            if (!inputFile.is_open()) {
+                cout << "Nepavyko atidaryti failo. Bandykite dar karta.\n";
+            }
+            else {
+                inputFile.close();
+                skaitytiIsFailo(studentai, failoPavadinimas);
+                break;
+            }
+        } while (true);
+
         break;
     }
     case 2: {
@@ -40,9 +66,6 @@ int main() {
         addStudentsRandomly(studentai);
         break;
     }
-    default:
-        cout << "Neteisingas pasirinkimas.\n";
-        return 1;
     }
 
     if (studentai.empty()) {
