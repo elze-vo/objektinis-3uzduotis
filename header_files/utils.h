2652bf8e638bdde4e62553cb8f17f3d752418e4a
elze-vo <evoveriunaite@gmail.com>
2024-10-08 15:34:17 +0300

    Testinių failų kūrimas
    - Pridėta galimybė kurti testinius studentų failus.
    - Testinių failų kūrimo laikas yra skaičiuojamas.

--- header_files/utils.h
+++ header_files/utils.h
@@ -3,12 +3,14 @@
 
 #pragma
 #include <vector>
+#include <string>
 #include "Studentas.h"
 
 double calculateMedian(std::vector<int>& grades);
 bool compareByResultsAscending(const Studentas& a, const Studentas& b);
 bool compareByResultsDescending(const Studentas& a, const Studentas& b);
 void printOrSaveResults(std::vector<Studentas>& studentai, int pasirinkimas, int outputOption);
+void createStudentFile(int studentCount, int gradeCount, const std::string& fileName);
 
 
 #endif // UTILS_H
