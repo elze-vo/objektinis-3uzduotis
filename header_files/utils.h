cc4490810f411288eb97cf3534e18fc5ccebb9db
elze-vo <evoveriunaite@gmail.com>
2024-10-10 16:30:46 +0300

    Supaprastinas kodas
    - Duomenų apdirbimas sucentralizuotas.
    - Pridėta daugiau error-catching'o.

--- header_files/utils.h
+++ header_files/utils.h
@@ -11,6 +11,7 @@ bool compareByResultsAscending(const Studentas& a, const Studentas& b);
 bool compareByResultsDescending(const Studentas& a, const Studentas& b);
 void printOrSaveResults(std::vector<Studentas>& studentai, int pasirinkimas, int outputOption);
 void createStudentFile(int studentCount, int gradeCount, const std::string& fileName);
+void processStudentData(std::vector<Studentas>& studentai);
 
 
 #endif // UTILS_H
