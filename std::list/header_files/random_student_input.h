3ab2a88f449c43fef3f3a333611ef349bf783c5e
elze-vo <evoveriunaite@gmail.com>
2024-10-22 20:12:50 +0300

    Pridėti std::list failai

---
+++ std::list/header_files/random_student_input.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_STUDENT_INPUT_H
+#define RANDOM_STUDENT_INPUT_H
+
+#include "Studentas.h"
+#include <list>
+
+void addStudentsRandomly(std::list<Studentas>& studentai);
+
+#endif // RANDOM_STUDENT_INPUT_H
