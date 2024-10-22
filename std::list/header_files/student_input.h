b6ea7649e535839d8224601dd9c97df5ed143d2c
elze-vo <evoveriunaite@gmail.com>
2024-10-22 20:12:17 +0300

    Pridėti std::list failai

---
+++ std::list/header_files/student_input.h
@@ -0,0 +1,9 @@
+#ifndef STUDENT_INPUT_H
+#define STUDENT_INPUT_H
+
+#include "Studentas.h"
+#include <list>
+
+void addStudentsManually(std::list<Studentas>& studentai);
+
+#endif // STUDENT_INPUT_H
