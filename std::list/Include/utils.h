e9bb20b8bc7ed0a8127eb6bb2818154a7ed55556
elze-vo <evoveriunaite@gmail.com>
2024-11-07 18:24:31 +0200

    Failų perkėlimas ir calculateFinalGrade, writeResultsToFile
    - Pridėtos dvi funkcijos calculateFinalGrade ir writeResultsToFile.

--- std::list/Include/utils.h
+++ std::list/Include/utils.h
@@ -1,17 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#pragma
 #include <list>
 #include <string>
 #include "Studentas.h"
 
 double calculateMedian(std::list<int>& grades);
+double calculateFinalGrade(const Studentas& studentas, int pasirinkimas);
 bool compareByResultsAscending(const Studentas& a, const Studentas& b);
 bool compareByResultsDescending(const Studentas& a, const Studentas& b);
 void printOrSaveResults(std::list<Studentas>& studentai, int pasirinkimas, int outputOption);
 void createStudentFile(int studentCount, int gradeCount, const std::string& fileName);
 void processStudentData(std::list<Studentas>& studentai);
-
+void writeResultsToFile(std::list<Studentas>& studentai, const std::string& filename, int pasirinkimas, int sortOption);
 
 #endif // UTILS_H
