0cd5d492b64e6b94fa7ec2538dcbadcddee3fa17
elze-vo <evoveriunaite@gmail.com>
2024-11-07 16:39:37 +0200

    Failų perkėlimas ir calculateFinalGrade
    - Pridėta funkcija calculateFinalGrade.

--- std::vector/Include/utils.h
+++ std::vector/Include/utils.h
@@ -7,11 +7,12 @@
 #include "Studentas.h"
 
 double calculateMedian(std::vector<int>& grades);
+double calculateFinalGrade(const Studentas& studentas, int pasirinkimas);
 bool compareByResultsAscending(const Studentas& a, const Studentas& b);
 bool compareByResultsDescending(const Studentas& a, const Studentas& b);
 void printOrSaveResults(std::vector<Studentas>& studentai, int pasirinkimas, int outputOption);
 void createStudentFile(int studentCount, int gradeCount, const std::string& fileName);
 void processStudentData(std::vector<Studentas>& studentai);
-
+void writeResultsToFile(std::vector<Studentas>& studentai, const std::string& filename, int pasirinkimas, int sortOption);
 
 #endif // UTILS_H
