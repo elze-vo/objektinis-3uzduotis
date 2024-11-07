2b30dc70060f814ae85dc2e889d14b9c74dea170
elze-vo <evoveriunaite@gmail.com>
2024-11-07 16:52:07 +0200

    Pridėtas run.bat failas

---
+++ std::list/run.bat
@@ -0,0 +1,11 @@
+cmake -DCMAKE_BUILD_TYPE=Release .
+cmake --install . --config Release
+cmake --build . --config Release
+copy src\stud1000.txt Release\stud1000.txt
+copy src\stud10000.txt Release\stud10000.txt
+copy src\stud100000.txt Release\stud100000.txt
+copy src\stud1000000.txt Release\stud1000000.txt
+copy src\stud10000000.txt Release\stud10000000.txt
+cd Release
+studentuAppsasList.exe
+pause
