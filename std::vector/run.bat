6af297e31ff7afe54267a8c726bd01d8d5f71296
elze-vo <evoveriunaite@gmail.com>
2024-11-07 21:02:15 +0200

    run.bat pritaikytas testavimo failams

--- std::vector/run.bat
+++ std::vector/run.bat
@@ -4,8 +4,6 @@ cmake --build . --config Release
 copy src\stud1000.txt Release\stud1000.txt
 copy src\stud10000.txt Release\stud10000.txt
 copy src\stud100000.txt Release\stud100000.txt
-copy src\stud1000000.txt Release\stud1000000.txt
-copy src\stud10000000.txt Release\stud10000000.txt
 cd Release
 studentuAppsas.exe
 pause
