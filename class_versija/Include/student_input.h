967041e074e789db8f21521ad7b4678e9118a071
elze-vo <evoveriunaite@gmail.com>
2024-11-20 19:18:22 +0200

    Atnaujintas student_input.h failas

--- class_versija/Include/student_input.h
+++ class_versija/Include/student_input.h
@@ -1,10 +1,9 @@
 #ifndef STUDENT_INPUT_H
 #define STUDENT_INPUT_H
 
-#include "Studentas.h"  // Include the updated Studentas class
+#include "Studentas.h"
 #include <vector>
 
-// Function to add students manually to the provided vector
 void addStudentsManually(std::vector<Studentas>& studentai);
 
 #endif // STUDENT_INPUT_H
