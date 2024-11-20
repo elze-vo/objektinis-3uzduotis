53ba9d66b88f2dfb91b5c4eef9a21db25c77317d
elze-vo <evoveriunaite@gmail.com>
2024-11-20 22:00:36 +0200

    Perkeliami failai

--- class_versija/Include/random_student_input.h
+++
@@ -1,9 +0,0 @@
-#ifndef RANDOM_STUDENT_INPUT_H
-#define RANDOM_STUDENT_INPUT_H
-
-#include "Studentas.h"
-#include <vector>
-
-void addStudentsRandomly(std::vector<Studentas>& studentai);
-
-#endif // RANDOM_STUDENT_INPUT_H
--- class_versija/Include/student_input.h
+++
@@ -1,9 +0,0 @@
-#ifndef STUDENT_INPUT_H
-#define STUDENT_INPUT_H
-
-#include "Studentas.h"
-#include <vector>
-
-void addStudentsManually(std::vector<Studentas>& studentai);
-
-#endif // STUDENT_INPUT_H
