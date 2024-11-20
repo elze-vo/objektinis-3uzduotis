a90fa6ba050ce2563e509effedeeeabe6242125c
elze-vo <evoveriunaite@gmail.com>
2024-11-20 22:19:09 +0200

    Pataisytas run.bat

--- std_list/run.bat
+++ std_list/run.bat
@@ -3,7 +3,5 @@ cmake --install . --config Release
 cmake --build . --config Release
 copy src\stud1000.txt Release\stud1000.txt
 copy src\stud10000.txt Release\stud10000.txt
-copy src\stud100000.txt Release\stud100000.txt
 cd Release
-studentuAppsasList.exe
 pause
