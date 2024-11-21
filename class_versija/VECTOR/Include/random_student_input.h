bda94131f1c0b06394fa3ce94a3c07d894e047c8
elze-vo <evoveriunaite@gmail.com>
2024-11-21 15:18:27 +0200

    Pataisytas random_student_input.h failas

--- class_versija/VECTOR/Include/random_student_input.h
+++ class_versija/VECTOR/Include/random_student_input.h
@@ -1,10 +1,9 @@
 #ifndef RANDOM_STUDENT_INPUT_H
 #define RANDOM_STUDENT_INPUT_H
 
-#include "Studentas.h"  // Ensure this includes the class declaration for Studentas
+#include "Studentas.h"
 #include <vector>
 
-// Function declaration to add students randomly
 void addStudentsRandomly(std::vector<Studentas>& studentai);
 
 #endif // RANDOM_STUDENT_INPUT_H
