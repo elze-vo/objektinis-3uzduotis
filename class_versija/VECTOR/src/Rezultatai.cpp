d6c9265e6a73f02d294a1a203264328afb9c2f17
elze-vo <evoveriunaite@gmail.com>
2024-11-21 15:20:12 +0200

    Pataisytas Rezultatai.cpp failas

--- class_versija/VECTOR/src/Rezultatai.cpp
+++ class_versija/VECTOR/src/Rezultatai.cpp
@@ -3,16 +3,14 @@
 #include <sstream>
 
 std::istream& operator>>(std::istream& is, Rezultatai& rezultatai) {
-    // Read homework results (assuming they're space-separated integers)
     int grade;
     while (is >> grade) {
         if (grade < 0 || grade > 10) {
-            break;  // Exit when we encounter an invalid grade
+            break;
         }
         rezultatai.namuDarbuRezultatai.push_back(grade);
     }
 
-    // Read the exam result
     is >> rezultatai.egzaminoRezultatas;
 
     return is;
