777b5650c6d08c5100f56418f8985e6bfdfc624a
elze-vo <evoveriunaite@gmail.com>
2024-12-05 21:10:00 +0200

    Pataisytas utils.h failas

--- class_versija/VECTOR/Include/utils.h
+++ class_versija/VECTOR/Include/utils.h
@@ -6,6 +6,12 @@
 #include <string>
 #include "Studentas.h"
 
+void demonstruotiTrisMetodus();
+
+void correctInvalidData(int& rezultatas, const std::string& fieldName);
+
+void skaitytiIsFailo(std::vector<Studentas>& studentai, const std::string& fileName);
+
 double calculateMedian(std::vector<double>& grades);
 
 double calculateFinalGrade(const Studentas& studentas, int pasirinkimas);
@@ -14,12 +20,12 @@ bool compareByResultsAscending(const Studentas& a, const Studentas& b);
 
 bool compareByResultsDescending(const Studentas& a, const Studentas& b);
 
-void printOrSaveResults(std::vector<Studentas>& studentai, int pasirinkimas, int outputOption);
+void printOrSaveResults(const std::vector<Studentas>& studentai, int pasirinkimas, int outputOption);
 
 void createStudentFile(int studentCount, int gradeCount, const std::string& fileName);
 
 void processStudentData(std::vector<Studentas>& studentai);
 
-void writeResultsToFile(std::vector<Studentas>& studentai, const std::string& filename, int pasirinkimas, int sortOption);
+void writeResultsToFile(const std::vector<Studentas>& studentai, const std::string& filename, int pasirinkimas);
 
 #endif // UTILS_H
