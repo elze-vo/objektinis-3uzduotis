802128d96a1fa78c5d1b408297e69e696fe4b3ee
elze-vo <evoveriunaite@gmail.com>
2024-12-05 21:13:40 +0200

    Pataisytas main.cpp failas

--- class_versija/LIST/src/main.cpp
+++ class_versija/LIST/src/main.cpp
@@ -8,7 +8,6 @@
 #include <chrono>
 #include <cmath>
 #include "Studentas.h"
-#include "Rezultatai.h"
 #include "utils.h"
 #include "student_input.h"
 #include "strategijos.h"
@@ -28,10 +27,11 @@ int main() {
         cout << "3 - Generuoti studentus atsitiktinai\n";
         cout << "4 - Sukurti studentu faila\n";
         cout << "5 - Testuoti programos greiti\n";
+        cout << "6 - Demonstruoti tris metodus\n";
         cout << "Iveskite pasirinkima: ";
         cin >> pasirinkimas;
 
-        if (cin.fail() || (pasirinkimas < 1 || pasirinkimas > 5)) {
+        if (cin.fail() || (pasirinkimas < 1 || pasirinkimas > 6)) {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
@@ -58,20 +58,6 @@ int main() {
             else {
                 inputFile.close();
                 skaitytiIsFailo(studentai, failoPavadinimas);
-
-                int gradeCalculationOption;
-                do {
-                    cout << "Pasirinkite, kaip skaiciuoti galutini bala:\n";
-                    cout << "1 - Vidurkis\n";
-                    cout << "2 - Mediana\n";
-                    cout << "Iveskite pasirinkima: ";
-                    cin >> gradeCalculationOption;
-                } while (gradeCalculationOption < 1 || gradeCalculationOption > 2);
-
-                for (auto& studentas : studentai) {
-                    studentas.setFinalGrade(calculateFinalGrade(studentas, gradeCalculationOption));
-                }
-
                 processStudentData(studentai);
                 break;
             }
@@ -80,42 +66,15 @@ int main() {
         system("pause");
         break;
     }
+
     case 2: {
         addStudentsManually(studentai);
-
-        int gradeCalculationOption;
-        do {
-            cout << "Pasirinkite, kaip skaiciuoti galutini bala:\n";
-            cout << "1 - Vidurkis\n";
-            cout << "2 - Mediana\n";
-            cout << "Iveskite pasirinkima: ";
-            cin >> gradeCalculationOption;
-        } while (gradeCalculationOption < 1 || gradeCalculationOption > 2);
-
-        for (auto& studentas : studentai) {
-            studentas.setFinalGrade(calculateFinalGrade(studentas, gradeCalculationOption));
-        }
-
         processStudentData(studentai);
         system("pause");
         break;
     }
     case 3: {
         addStudentsRandomly(studentai);
-
-        int gradeCalculationOption;
-        do {
-            cout << "Pasirinkite, kaip skaiciuoti galutini bala:\n";
-            cout << "1 - Vidurkis\n";
-            cout << "2 - Mediana\n";
-            cout << "Iveskite pasirinkima: ";
-            cin >> gradeCalculationOption;
-        } while (gradeCalculationOption < 1 || gradeCalculationOption > 2);
-
-        for (auto& studentas : studentai) {
-            studentas.setFinalGrade(calculateFinalGrade(studentas, gradeCalculationOption));
-        }
-
         processStudentData(studentai);
         system("pause");
         break;
@@ -139,64 +98,52 @@ int main() {
         return 0;
     }
     case 5: {
-        std::string failoPavadinimas;
+        string failoPavadinimas;
         char pasirinkimasTestuoti;
 
-        std::list<Studentas> neislaike;
-        std::list<Studentas> islaike;
+        list<Studentas> neislaike;
+        list<Studentas> islaike;
 
         do {
-            std::cout << "Iveskite failo pavadinima (pvz., studentai.txt): ";
-            std::cin >> failoPavadinimas;
-
-            std::list<Studentas> studentai;
-            skaitytiIsFailo(studentai, failoPavadinimas);
-
-            int gradeCalculationOption;
-            do {
-                std::cout << "Pasirinkite, kaip skaiciuoti galutini bala:\n";
-                std::cout << "1 - Vidurkis\n";
-                std::cout << "2 - Mediana\n";
-                std::cout << "Iveskite pasirinkima: ";
-                std::cin >> gradeCalculationOption;
-            } while (gradeCalculationOption < 1 || gradeCalculationOption > 2);
-
-            for (auto& studentas : studentai) {
-                studentas.setFinalGrade(calculateFinalGrade(studentas, gradeCalculationOption));
-            }
+            cout << "Iveskite failo pavadinima (pvz., studentai.txt): ";
+            cin >> failoPavadinimas;
 
             int strategyOption;
             do {
-                std::cout << "Pasirinkite, kuria strategija naudoti studentu skaidymui:\n";
-                std::cout << "1 - Strategija 1 (kopijuojant i du naujus list)\n";
-                std::cout << "2 - Strategija 2 (perkeliant 'neislaike' ir trinant is originalaus list)\n";
-                std::cout << "3 - Strategija 3 (naudojant std::list::splice)\n";
-                std::cout << "4 - Lyginti visas strategijas\n";
-                std::cout << "Iveskite pasirinkima: ";
-                std::cin >> strategyOption;
-            } while (strategyOption < 1 || strategyOption > 4);
+                cout << "Pasirinkite, kuria strategija naudoti studentu skaidymui:\n";
+                cout << "1 - Strategija 1 (kopijuojant i du naujus vektorius)\n";
+                cout << "2 - Strategija 2 (perkeliant 'neislaike' ir trinant is originalaus vektoriaus)\n";
+                cout << "3 - Strategija 3 (naudojant std::partition)\n";
+                cout << "4 - Lyginti visas strategijas\n";
+                cout << "Iveskite pasirinkima: ";
+                cin >> strategyOption;
 
-            int sortOption;
-            do {
-                std::cout << "Pasirinkite rezultatu rikiavimo tvarka:\n";
-                std::cout << "1 - Rikiuoti didejimo tvarka\n";
-                std::cout << "2 - Rikiuoti mazejimo tvarka\n";
-                std::cout << "Iveskite pasirinkima: ";
-                std::cin >> sortOption;
-
-                if (std::cin.fail() || (sortOption < 1 || sortOption > 2)) {
-                    std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    std::cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+                if (cin.fail() || (strategyOption < 1 || strategyOption > 4)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
                 }
                 else {
                     break;
                 }
             } while (true);
 
+            list<Studentas> studentai;
+
+            skaitytiIsFailo(studentai, failoPavadinimas);
+
+            processStudentData(studentai);
+
             if (strategyOption == 4) {
-                std::list<Studentas> temp = studentai;
+                std::list<Studentas> temp;
+
+                for (auto& studentas : studentai) {
+                    studentas.setFinalGrade(calculateFinalGrade(studentas, pasirinkimas));
+                }
+
                 for (int i = 1; i < 4; ++i) {
+                    temp = studentai;
+
                     switch (i) {
                     case 1: strategija1(temp, neislaike, islaike); break;
                     case 2: strategija2(temp, neislaike); break;
@@ -204,34 +151,52 @@ int main() {
                     }
                 }
             }
+
             else {
+                for (auto& studentas : studentai) {
+                    studentas.setFinalGrade(calculateFinalGrade(studentas, pasirinkimas));
+                }
+
                 if (strategyOption == 1) {
                     strategija1(studentai, neislaike, islaike);
+                    writeResultsToFile(neislaike, "neislaike.txt", pasirinkimas);
+                    writeResultsToFile(islaike, "islaike.txt", pasirinkimas);
                 }
                 else if (strategyOption == 2) {
                     strategija2(studentai, neislaike);
+                    writeResultsToFile(neislaike, "neislaike.txt", pasirinkimas);
+                    writeResultsToFile(studentai, "islaike.txt", pasirinkimas);
                 }
                 else if (strategyOption == 3) {
                     strategija3(studentai, neislaike);
+                    writeResultsToFile(neislaike, "neislaike.txt", pasirinkimas);
+                    writeResultsToFile(studentai, "islaike.txt", pasirinkimas);
                 }
 
-                writeResultsToFile(neislaike, "neislaike.txt", sortOption);
-                writeResultsToFile(islaike, "islaie.txt", sortOption);
             }
-
-            std::cout << "Ar norite atidaryti kita faila greicio testavimui? (y/n): ";
-            std::cin >> pasirinkimasTestuoti;
+            cout << "Ar norite atidaryti kita faila greicio testavimui? (y/n): ";
+            cin >> pasirinkimasTestuoti;
 
             while (pasirinkimasTestuoti != 'y' && pasirinkimasTestuoti != 'n') {
-                std::cout << "Neteisingas pasirinkimas. Iveskite 'y' arba 'n': ";
-                std::cin >> pasirinkimasTestuoti;
+                cout << "Neteisingas pasirinkimas. Iveskite 'y' arba 'n': ";
+                cin >> pasirinkimasTestuoti;
             }
+
             if (pasirinkimasTestuoti == 'y') {
                 studentai.clear();
             }
         } while (pasirinkimasTestuoti == 'y');
         return 0;
     }
+
+    case 6: {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        demonstruotiTrisMetodus();
+        system("pause");
+        break;
     }
+    }
+
     return 0;
 }
