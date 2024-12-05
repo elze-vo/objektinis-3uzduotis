13cea32f4f2c6738f485fc9001481ff41a874204
elze-vo <evoveriunaite@gmail.com>
2024-12-05 21:08:51 +0200

    Pataisytas Studentas.h failas

--- class_versija/VECTOR/Include/Studentas.h
+++ class_versija/VECTOR/Include/Studentas.h
@@ -3,38 +3,73 @@
 
 #include <string>
 #include <vector>
-#include "Rezultatai.h"
+#include <iostream>
+#include <iomanip>
 
 class Studentas {
 private:
     std::string vardas;
     std::string pavarde;
-    Rezultatai rezultatai;
+    std::vector<double> namuDarbuRezultatai;
+    double egzaminoRezultatas;
     double finalGrade;
 
 public:
-    Studentas() : finalGrade(0) {}
+    // Constructors
+    Studentas() : egzaminoRezultatas(0), finalGrade(0) {}
 
-    Studentas(const std::string& vardas, const std::string& pavarde, const Rezultatai& rezultatai, double finalGrade)
-        : vardas(vardas), pavarde(pavarde), rezultatai(rezultatai), finalGrade(finalGrade) {}
+    Studentas(const std::string& vardas, const std::string& pavarde,
+        const std::vector<double>& ndRezultatai, double egzRezultatas, double finalGrade)
+        : vardas(vardas), pavarde(pavarde), namuDarbuRezultatai(ndRezultatai),
+        egzaminoRezultatas(egzRezultatas), finalGrade(finalGrade) {}
 
-    ~Studentas() = default;
+    // Copy Constructor
+    Studentas(const Studentas& other)
+        : vardas(other.vardas), pavarde(other.pavarde),
+        namuDarbuRezultatai(other.namuDarbuRezultatai),
+        egzaminoRezultatas(other.egzaminoRezultatas), finalGrade(other.finalGrade) {}
 
+    // Copy Assignment Operator
+    Studentas& operator=(const Studentas& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            namuDarbuRezultatai = other.namuDarbuRezultatai;
+            egzaminoRezultatas = other.egzaminoRezultatas;
+            finalGrade = other.finalGrade;
+        }
+        return *this;
+    }
+
+    // Destructor
+    ~Studentas() {
+        vardas.clear();
+        pavarde.clear();
+        namuDarbuRezultatai.clear();
+    }
+
+    // Accessor and Mutator Methods for Student Information
     std::string getVardas() const { return vardas; }
     std::string getPavarde() const { return pavarde; }
-    const Rezultatai& getRezultatai() const { return rezultatai; }
-    Rezultatai& getRezultatai() { return rezultatai; }
-    double getFinalGrade() const { return finalGrade; }
-
     void setVardas(const std::string& vardas) { this->vardas = vardas; }
     void setPavarde(const std::string& pavarde) { this->pavarde = pavarde; }
-    void setRezultatai(const Rezultatai& rezultatai) { this->rezultatai = rezultatai; }
+
+    // Accessor and Mutator Methods for Results
+    std::vector<double>& getNamuDarbuRezultatai() { return namuDarbuRezultatai; }
+    const std::vector<double>& getNamuDarbuRezultatai() const { return namuDarbuRezultatai; }
+    double getEgzaminoRezultatas() const { return egzaminoRezultatas; }
+    void setNamuDarbuRezultatai(const std::vector<double>& ndRezultatai) {
+        this->namuDarbuRezultatai = ndRezultatai;
+    }
+    void setEgzaminoRezultatas(double egzRezultatas) { this->egzaminoRezultatas = egzRezultatas; }
+
+    // Accessor and Mutator Methods for Final Grade
+    double getFinalGrade() const { return finalGrade; }
     void setFinalGrade(double finalGrade) { this->finalGrade = finalGrade; }
 
+    // Input and Output Operators
     friend std::istream& operator>>(std::istream& is, Studentas& studentas);
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& studentas);
 };
-
-void correctInvalidData(int& rezultatas, const std::string& fieldName);
-void skaitytiIsFailo(std::vector<Studentas>& studentai, const std::string& fileName);
 
 #endif // STUDENTAS_H
