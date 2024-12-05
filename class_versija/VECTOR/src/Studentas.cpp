6cc853737aa44b5abeacfe79cd065c6e354880c0
elze-vo <evoveriunaite@gmail.com>
2024-12-05 21:03:23 +0200

    Atnaujintas Studentas.cpp failas

--- class_versija/VECTOR/src/Studentas.cpp
+++ class_versija/VECTOR/src/Studentas.cpp
@@ -4,107 +4,130 @@
 #include <vector>
 #include <string>
 #include <limits>
+#include <iomanip>
 #include "Studentas.h"
-#include "utils.h"
 
 using namespace std;
 
 std::istream& operator>>(std::istream& is, Studentas& studentas) {
-    std::string vardas, pavarde;
-    is >> vardas >> pavarde;
-    studentas.setVardas(vardas);
-    studentas.setPavarde(pavarde);
+    if (&is == &std::cin) {
+        // Manual input handling
+        cout << "Iveskite studento varda: ";
+        string vardas;
+        cin >> vardas;
+        studentas.setVardas(vardas);
 
-    Rezultatai rezultatai;
-    is >> rezultatai;
+        cout << "Iveskite studento pavarde: ";
+        string pavarde;
+        cin >> pavarde;
+        studentas.setPavarde(pavarde);
+        cin.ignore();
 
-    studentas.setRezultatai(rezultatai);
+        cout << "Iveskite namu darbu rezultatus (dukart spauskite Enter norint baigti): " << endl;
+        vector<double> homeworkGrades;
+        string input;
 
+        while (true) {
+            cout << "Namu darbas: ";
+            getline(cin, input);
+
+            if (input.empty()) {
+                if (!homeworkGrades.empty()) break;
+                cout << "Namu darbu rezultatai negali buti tusti. Bandykite dar karta." << endl;
+            }
+            else {
+                try {
+                    int grade = stoi(input);
+                    if (grade < 0 || grade > 10) {
+                        cout << "Namu darbas turi buti tarp 0 ir 10. Bandykite dar karta." << endl;
+                    }
+                    else {
+                        homeworkGrades.push_back(static_cast<double>(grade));
+                    }
+                }
+                catch (invalid_argument&) {
+                    cout << "Ivedete neteisinga reiksme. Bandykite dar karta." << endl;
+                }
+                catch (out_of_range&) {
+                    cout << "Ivedete reiksme, kuri yra per didele. Bandykite dar karta." << endl;
+                }
+            }
+        }
+        studentas.setNamuDarbuRezultatai(homeworkGrades);
+
+        cout << "Iveskite egzamino rezultata: ";
+        while (true) {
+            string examInput;
+            cin >> examInput;
+
+            try {
+                int examGrade = stoi(examInput);
+                if (examGrade < 0 || examGrade > 10) {
+                    cout << "Egzamino rezultatas turi buti tarp 0 ir 10. Bandykite dar karta: ";
+                }
+                else {
+                    studentas.setEgzaminoRezultatas(examGrade);
+                    break;
+                }
+            }
+            catch (invalid_argument&) {
+                cout << "Ivedete neteisinga reiksme. Bandykite dar karta." << endl;
+            }
+            catch (out_of_range&) {
+                cout << "Ivedete reiksme, kuri yra per didele. Bandykite dar karta." << endl;
+            }
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+    else {
+        // File input handling
+        string vardas, pavarde;
+        is >> vardas >> pavarde;
+
+        if (is.fail()) return is;
+
+        std::vector<double> homeworkGrades;
+        double grade;
+
+        for (int i = 0; i < 5; ++i) {
+            if (is >> grade) {
+                if (grade < 0 || grade > 10) {
+                    is.setstate(std::ios::failbit);
+                    return is;
+                }
+                homeworkGrades.push_back(grade);
+            }
+            else {
+                is.setstate(std::ios::failbit);
+                return is;
+            }
+        }
+
+        double egzaminas;
+        if (is >> egzaminas) {
+            if (egzaminas < 0 || egzaminas > 10) {
+                is.setstate(std::ios::failbit);
+                return is;
+            }
+        }
+        else {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        studentas.setVardas(vardas);
+        studentas.setPavarde(pavarde);
+        studentas.setNamuDarbuRezultatai(homeworkGrades);
+        studentas.setEgzaminoRezultatas(egzaminas);
+    }
     return is;
 }
 
-void correctInvalidData(int& rezultatas, const string& fieldName) {
-    if (rezultatas < 0 || rezultatas > 10) {
-        cout << "Invalid " << fieldName << ": " << rezultatas << ". Using default value 0." << endl;
-        rezultatas = 0;
-    }
-}
-
-void skaitytiIsFailo(vector<Studentas>& studentai, const string& fileName) {
-    ifstream file(fileName);
-    if (!file) {
-        cout << "Nepavyko atidaryti failo: " << fileName << endl;
-        return;
-    }
-
-    string line;
-    getline(file, line);
-
-    istringstream headerStream(line);
-    string headerWord;
-    int numHomework = 0;
-    while (headerStream >> headerWord) {
-        if (headerWord.find("ND") != string::npos) {
-            numHomework++;
-        }
-    }
-
-    while (getline(file, line)) {
-        line.erase(0, line.find_first_not_of(" \t"));
-        line.erase(line.find_last_not_of(" \t") + 1);
-
-        istringstream studentStream(line);
-        Studentas studentas;
-
-        string vardas, pavarde;
-        if (!(studentStream >> vardas >> pavarde)) {
-            cout << "Klaida skaitant studento varda ir pavarde" << endl;
-            continue;
-        }
-        studentas.setVardas(vardas);
-        studentas.setPavarde(pavarde);
-
-        vector<double> namuDarbai;
-        namuDarbai.reserve(numHomework);
-
-        for (int i = 0; i < numHomework; ++i) {
-            string temp;
-            if (!(studentStream >> temp)) {
-                cout << "Klaida skaitant namu darbo rezultata" << endl;
-                break;
-            }
-            try {
-                double rezultatas = stod(temp);
-                if (rezultatas < 0 || rezultatas > 10) {
-                    throw out_of_range("Invalid value");
-                }
-                namuDarbai.push_back(rezultatas);
-            }
-            catch (const exception&) {
-                cout << "Netinkamas namu darbo rezultatas: " << temp << endl;
-                namuDarbai.push_back(0.0);
-            }
-        }
-
-        studentas.getRezultatai().setNamuDarbuRezultatai(namuDarbai);
-
-        string tempExam;
-        if (studentStream >> tempExam) {
-            try {
-                double egzaminas = stod(tempExam);
-                if (egzaminas < 0 || egzaminas > 10) {
-                    throw out_of_range("Invalid value");
-                }
-                studentas.getRezultatai().setEgzaminoRezultatas(egzaminas);
-            }
-            catch (const exception&) {
-                cout << "Netinkamas egzamino rezultatas: " << tempExam << endl;
-                studentas.getRezultatai().setEgzaminoRezultatas(0.0);
-            }
-        }
-
-        studentai.push_back(studentas);
-    }
-
-    file.close();
+std::ostream& operator<<(std::ostream& os, const Studentas& studentas) {
+    os << std::setw(15) << std::left << studentas.getVardas()
+        << std::setw(15) << std::left << studentas.getPavarde()
+        << std::setw(25) << std::left << &studentas
+        << std::fixed << std::setprecision(2) << studentas.getFinalGrade();
+    return os;
 }
